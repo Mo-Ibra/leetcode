27c3cba472d6195754a085065413b50c716f11f4
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-22 14:45:32 +0200

    Time: 135 ms (11.31%), Space: 46 MB (19.02%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+
+    let up = 0;
+    let right = 0;
+
+    let myArray = moves.split('');
+
+    myArray.forEach(move => {
+        switch(move){
+            case 'U':
+                up++;
+                break;
+            case 'D':
+                up--;
+                break;
+            case 'R':
+                right++;
+                break;
+            case 'L':
+                right--;
+                break;
+            default:
+                '';
+        }
+    });
+
+    console.log(up, right);
+
+    if (up == 0 && right == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
