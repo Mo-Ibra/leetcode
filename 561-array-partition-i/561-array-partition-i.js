90bea15b05ec999315122f3604e246c2ada84fc2
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-24 14:23:01 +0200

    Time: 491 ms (5.01%), Space: 50.7 MB (7.44%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.js
+++ 561-array-partition-i/561-array-partition-i.js
@@ -15,28 +15,19 @@ Array.prototype.chunk = function (size) {
 }
 
 var arrayPairSum = function (nums) {
-        // sort number 
-        // get the small number of first two
-        // and small of second 
-        // and etc..
-        // return sum of all numbers;
-
+    
     let sortNums = nums.sort((a, b) => a - b);
     let chunk = sortNums.chunk(2);
 
     let arrayOfResult = [];
     let result = 0;
 
-    console.log(chunk);
-
-    console.log(chunk.length);
 
     for (let i = 0; i < chunk.length; i++) {
         arrayOfResult.push(Math.min(...chunk[i]));
         result += Math.min(...chunk[i]);
     }
 
-    console.log(result);
 
     return result;
 };
