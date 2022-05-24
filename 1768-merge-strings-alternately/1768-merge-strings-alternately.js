d94142b523c65b8bbfef5bf9e847420b5fa2a29f
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-24 13:41:52 +0200

    Time: 76 ms (54.25%), Space: 42.6 MB (35.37%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+
+    let result = [];
+
+    let firstArray = word1.split('');
+    let secondArray = word2.split('');
+
+    let getLenght = [firstArray.length, secondArray.length];
+
+    let maxLength = Math.max(...getLenght);
+
+    for (let i = 0; i < maxLength; i++) {
+        if (firstArray[i] !== undefined) {
+            result.push(firstArray[i]);
+        }
+        if (secondArray[i] !== undefined) {
+            result.push(secondArray[i]);
+        }
+    }
+
+    return result.join('');
+
+    console.log(result);
+};
