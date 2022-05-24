31c3725a2906518e417f91aef3f42ff971ca06b7
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-24 14:19:18 +0200

    Time: 170 ms (37.05%), Space: 47.7 MB (39.20%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = nums => {
+    nums.sort((a, b) => a - b);
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            sum += nums[i];
+        }
+    }
+
+    return sum
+};
