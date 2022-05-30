9a3cd12faa642ca79695fdc39ebec1d7837cb788
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-30 22:32:38 +0200

    Time: 104 ms (12.50%), Space: 47.1 MB (6.62%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -2,22 +2,42 @@
  * @param {number} n
  * @return {string}
  */
+// var generateTheString = function (n) {
+
+//     let randomCharacters = [];
+//     let countDeplicatedNumbers = 0;
+
+//     for (let i = 0; i < n; i++) {
+//         randomCharacters.push('a');
+//     }
+
+//     randomCharacters.forEach(() => {
+//         countDeplicatedNumbers = (countDeplicatedNumbers || 0) + 1;
+//     });
+
+//     if (countDeplicatedNumbers % 2 == 0 ) {
+//         randomCharacters[0] = 'b';
+//     }
+
+//     return randomCharacters.join('');
+// }
+
 var generateTheString = function (n) {
+    
+    let result = [];
 
-    let randomCharacters = [];
-    let countDeplicatedNumbers = 0;
-
-    for (let i = 0; i < n; i++) {
-        randomCharacters.push('a');
+    if (n % 2 == 0) {
+        for (let i = 0; i < n; i++) {
+            result.push('a');
+            result[0] = 'b';
+        }
+    } else {
+        for (let i = 0; i < n; i++) {
+            result.push('a');
+        }
     }
+    
+    console.log(result);
 
-    randomCharacters.forEach(() => {
-        countDeplicatedNumbers = (countDeplicatedNumbers || 0) + 1;
-    });
-
-    if (countDeplicatedNumbers % 2 == 0 ) {
-        randomCharacters[0] = 'b';
-    }
-
-    return randomCharacters.join('');
+    return result;
 }
