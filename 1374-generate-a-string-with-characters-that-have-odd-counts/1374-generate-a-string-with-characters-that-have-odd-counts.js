9e482501e73c2e0e70b70cf3136d237e121471cf
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-05-30 22:29:12 +0200

    Time: 153 ms (5.15%), Space: 46.8 MB (6.62%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var generateTheString = function (n) {
+
+    let randomCharacters = [];
+    let countDeplicatedNumbers = 0;
+
+    for (let i = 0; i < n; i++) {
+        randomCharacters.push('a');
+    }
+
+    randomCharacters.forEach(() => {
+        countDeplicatedNumbers = (countDeplicatedNumbers || 0) + 1;
+    });
+
+    console.log(randomCharacters);
+    console.log(countDeplicatedNumbers);
+
+    if (countDeplicatedNumbers % 2 == 0 ) {
+        randomCharacters[0] = 'b';
+    }
+    
+    console.log(randomCharacters.join(''));
+
+    return randomCharacters.join('');
+}
