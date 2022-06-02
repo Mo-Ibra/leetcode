31dc397e78d2142aa34c31312ace7444606e7f4a
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-03 00:09:45 +0200

    Time: 88 ms (46.52%), Space: 44.8 MB (17.07%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -7,13 +7,11 @@ var countNegatives = function (grid) {
     let result = 0;
 
     grid.forEach(num => {
-        if (Array.isArray(num)) {
-            num.forEach(n => {
-                if (n.toString().startsWith('-')) {
-                    result += 1;
-                }
-            });
-        }
+        num.forEach(n => {
+            if (n.toString().startsWith('-')) {
+                result += 1;
+            }
+        });
     });
 
     console.log(result);
