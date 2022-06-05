ae6c6e8a6f0c628777e42c1e71021cbfdb912827
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-05 21:01:43 +0200

    Time: 70 ms (71.92%), Space: 42.1 MB (61.15%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 var heightChecker = function (heights) {
+    
     let givenHight = [...heights];
     let sortedHight = heights.sort((a, b) => a - b);
 
     let theLength = sortedHight.length;
 
     let result = 0;
-
-    console.log(givenHight);
-    console.log(sortedHight);
-    console.log(theLength);
     
     for (let i = 0; i < theLength; i++) {
         if (givenHight[i] !== sortedHight[i]) {
@@ -20,6 +17,5 @@ var heightChecker = function (heights) {
         }
     };
     
-    console.log(result);
     return result;
 };
