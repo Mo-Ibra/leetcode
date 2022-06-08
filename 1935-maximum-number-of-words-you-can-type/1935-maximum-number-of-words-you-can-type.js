b93ea6d6d93b763db68222dc40fffc04bba1cf73
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-08 23:44:23 +0200

    Time: 203 ms (5.63%), Space: 48.5 MB (7.04%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
@@ -4,7 +4,6 @@
  * @return {number}
  */
 function arrayRemove(arr, value) {
-
     return arr.filter(function (ele) {
         return ele != value;
     });
@@ -14,18 +13,15 @@ var canBeTypedWords = function (text, brokenLetters) {
 
     let myArray = text.split(' ');
     let brokenWordsArray = brokenLetters.split('');
-
     let passed = text.split(' ');
 
     myArray.forEach(text => {
         for (let i = 0; i < brokenWordsArray.length; i++) {
             if (text.includes(brokenWordsArray[i])) {
-                console.log('Yes Its Include');
                 passed = arrayRemove(passed, text);
             }
         }
     });
 
-    console.log(passed.length);
     return passed.length;
 };
