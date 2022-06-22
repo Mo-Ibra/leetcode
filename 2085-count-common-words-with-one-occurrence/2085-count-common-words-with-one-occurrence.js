f6263480312b166253735c68b831544d00dfa037
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-21 20:18:40 -0700

    Time: 196 ms (8.40%), Space: 45.1 MB (90.46%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -0,0 +1,33 @@
+function checkTimesInArray(array, element) {
+
+    let count = 0;
+
+    array.forEach(ele => {
+        if (ele === element) {
+            count += 1;
+        }
+    });
+
+    return count;
+}
+
+
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+var countWords = function (words1, words2) {
+    
+    let result = 0;
+
+    words1.forEach(word1 => {
+        if (checkTimesInArray(words1, word1) == 1 && checkTimesInArray(words2, word1) == 1 ) {
+            result++;
+        }
+    });
+
+    console.log(result);
+    return result;
+
+};
