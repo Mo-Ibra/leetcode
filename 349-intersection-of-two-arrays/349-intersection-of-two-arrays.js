3df395c133d2012f6cf41f4c497b6a3297a35638
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-22 20:28:38 -0700

    Time: 106 ms (23.59%), Space: 42.7 MB (73.85%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -8,19 +8,12 @@ var intersection = function (nums1, nums2) {
     let result = [];
 
     nums1.forEach(num => {
-        console.log(nums2.indexOf(num));
         if (nums2.indexOf(num) != -1 ) {
             result.push(num);
         }
     });
 
-    let resultOfArray = [];
-
-    result.forEach(num => {
-        if (!resultOfArray.includes(num)) {
-            resultOfArray.push(num);
-        };
-    });
-
-    return resultOfArray;
+    let unique = [...new Set(result)];
+    
+    return unique;
 };
