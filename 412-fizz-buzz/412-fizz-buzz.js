de57c1f692b4a119a13bd1038ebbdb605922a82a
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-06-28 16:54:32 -0700

    Time: 99 ms (46.76%), Space: 44.6 MB (29.93%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -8,16 +8,12 @@ var fizzBuzz = function (n) {
 
     for (let i = 1; i <= n; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
-            console.log('Divid % 3 and % 5');
             result.push('FizzBuzz');
         } else if (i % 3 == 0) {
-            console.log('Divid % 3')
             result.push('Fizz');
         } else if (i % 5 == 0) {
-            console.log('Divid % 5');
             result.push('Buzz');
         } else {
-            console.log(i);
             result.push(i.toString());
         }
     };
