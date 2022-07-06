5206f03cec22f50a5e4454600079363d7f40e4cd
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-07-05 23:49:10 -0700

    Time: 115 ms (28.20%), Space: 46.1 MB (10.49%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -17,25 +17,15 @@ var majorityElement = function (nums) {
 
     const myObj = count;
 
-    // const maxValue = Math.max(...Object.values(myObj));
-
-    // console.log(myObj);
-    // console.log(maxValue);
-
     let myKey = 0;
     let maxValue = 0;
 
     for (const key in myObj) {
-        // console.log(key);
-        // console.log(myObj[key]);
         if (myObj[key] > maxValue) {
             myKey = key;
             maxValue = myObj[key]
         }
     };
-
-    console.log(Number(myKey));
-    // console.log(maxValue);
-
+    
     return Number(myKey);
 };
