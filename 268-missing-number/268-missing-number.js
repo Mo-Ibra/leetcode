a52e850287d5f42895d6ccf48cb84ecca0f2a597
Mohamed Ibrahim <70854249+Dev-Maradona@users.noreply.github.com>
2022-07-06 00:24:10 -0700

    Time: 113 ms (41.80%), Space: 44.6 MB (33.62%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -6,15 +6,11 @@ var missingNumber = function (nums) {
 
     let actualSum = nums.length * (nums.length + 1) / 2;
 
-    console.log(actualSum);
-
     let sumOfArray = 0;
 
     for (let i = 0; i < nums.length; i++) {
         sumOfArray = sumOfArray + nums[i];
     }
-
-    console.log(sumOfArray);
 
     return actualSum - sumOfArray;
 };
